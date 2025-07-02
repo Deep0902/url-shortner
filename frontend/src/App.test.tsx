97f9c902e0a04743fd4d38edbcd01e443c7c6ee5
Deep Rakesh <deeptank09@gmail.com>
2025-07-02 19:40:59 +0530

    package json update

---
+++ frontend/src/App.test.tsx
@@ -0,0 +1,20 @@
+jest.config.ts:
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+};
+
+src/App.test.tsx:
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
