8f8020eff1edde42a4dcbd1877df935f02210795
Deep Rakesh <deeptank09@gmail.com>
2025-07-04 09:42:41 +0530

    updated url for cors

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://url-shortner-amber-pi.vercel.app"
+    "https://shorty-nine-snowy.vercel.app"
   ],
   credentials: true
 }));
